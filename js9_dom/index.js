79d137113e7fd8996a0e5191410101ce11be4cb6
smack-witch-doctor <160270838+smack-witch-doctor@users.noreply.github.com>
2024-05-08 19:28:06 +0200

    Add files via upload

---
+++ js9_dom/index.js
@@ -0,0 +1,48 @@
+/*
+var rijeke= ["Mreznica", "Dobra", "Korana", "Kupa","Piva"];
+
+var h1= document.createElement("h1");
+h1.appendChild(document.createTextNode("Karlovac"));
+
+var bodyElem=document.querySelector("body");
+bodyElem.prepend(h1);
+----nadi div element s klasom opis
+var dDecsr = document.querySelector(".description");
+dDescr.innerHTML = "Grad na <h3> 5 rijeka <h3>";
+
+var infoDiv.document.createElement("div");
+infoDiv.classList.add("info");
+infoDiv.innerText="Peta rijeka je Piva";-direktno
+
+
+
+*/
+//dom manipulacije
+//turn ul into ol 
+/*
+var ul=document.querySelector("ul");
+ul.style.listStyle= "decimal";
+
+
+For petlja
+for(let elem of ul.children){
+
+    elem.style.listStyle= "decimal";
+    elem.textContent= i+ "."  + elem.textContent;
+i++;
+}
+
+var divInfo=document.createElement("div");
+divInfo.classList.add("sakrij");
+
+
+//saznaj marginu postavljenu od browsera (preza)
+
+var body =document.querySelector("body");
+var bodyW=window.getComputedStyle(body, null);
+var marginB=bodyW.getPropretyValue("margin-left");
+console.log("marginB");
+
+
+
+*/
